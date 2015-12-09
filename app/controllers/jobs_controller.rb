8296f43241529a22aef6b5b2d1048d906ce9a140
Cezar e Claudevan <cezarvaz@gmail.com>
2015-12-09 20:59:10 -0200

    Inclusão da funcionalidade para criar vagas

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,16 @@
 class JobsController < ApplicationController
+
   def show
     @job = Job.find(params[:id])
   end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.create(params.require(:job).permit(:title, :company, :location, :category, :description))
+    redirect_to @job
+  end
+
 end
